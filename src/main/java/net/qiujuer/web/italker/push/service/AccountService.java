885fae2bfd5044630ece8e2d1247d046c3ed3d30
JeffYe <531873600@qq.com>
2018-06-11 21:32:55 +0800

    web 第6.3章

--- src/main/java/net/qiujuer/web/italker/push/service/AccountService.java
+++ src/main/java/net/qiujuer/web/italker/push/service/AccountService.java
@@ -5,127 +5,138 @@ import net.qiujuer.web.italker.push.bean.api.account.AccountRspModel;
 import net.qiujuer.web.italker.push.bean.api.account.LoginModel;
 import net.qiujuer.web.italker.push.bean.api.account.RegisterModel;
 import net.qiujuer.web.italker.push.bean.api.base.ResponseModel;
-import net.qiujuer.web.italker.push.bean.card.UserCard;
 import net.qiujuer.web.italker.push.bean.db.User;
 import net.qiujuer.web.italker.push.factory.UserFactory;
-import net.qiujuer.web.italker.push.utils.Hib;
-import sun.rmi.runtime.Log;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
-import java.util.List;
 
 /**
  * @author qiujuer
  */
 // 127.0.0.1/api/account/...
 @Path("/account")
-public class AccountService {
-
-    //登录
+public class AccountService extends BaseService {
+    // 登录
     @POST
     @Path("/login")
     // 指定请求与返回的相应体为JSON
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public ResponseModel<AccountRspModel> login(LoginModel model){
-        if(!LoginModel.check(model)){
+    public ResponseModel<AccountRspModel> login(LoginModel model) {
+        if (!LoginModel.check(model)) {
+            // 返回参数异常
             return ResponseModel.buildParameterError();
         }
 
-        User user = UserFactory.login(model.getAccount(),model.getPassword());
-            if(user != null){
-                //如果有携带PushId
-                if(!Strings.isNullOrEmpty(model.getPushId())){
-                    return bind(user,model.getPushId());
-                }
 
-                AccountRspModel rspModel  = new AccountRspModel(user);
-                return  ResponseModel.buildOk(rspModel);
-            }else{
-                return ResponseModel.buildLoginError();
+        User user = UserFactory.login(model.getAccount(), model.getPassword());
+        if (user != null) {
+
+            // 如果有携带PushId
+            if (!Strings.isNullOrEmpty(model.getPushId())) {
+                return bind(user, model.getPushId());
             }
+
+            // 返回当前的账户
+            AccountRspModel rspModel = new AccountRspModel(user);
+            return ResponseModel.buildOk(rspModel);
+        } else {
+            // 登录失败
+            return ResponseModel.buildLoginError();
+        }
     }
 
+
+    // 注册
     @POST
     @Path("/register")
     // 指定请求与返回的相应体为JSON
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public ResponseModel<AccountRspModel> register(RegisterModel model) {
-        if(!RegisterModel.check(model)){
+        if (!RegisterModel.check(model)) {
+            // 返回参数异常
             return ResponseModel.buildParameterError();
         }
 
         User user = UserFactory.findByPhone(model.getAccount().trim());
-
         if (user != null) {
+            // 已有账户
             return ResponseModel.buildHaveAccountError();
         }
 
         user = UserFactory.findByName(model.getName().trim());
         if (user != null) {
-
+            // 已有用户名
             return ResponseModel.buildHaveNameError();
         }
 
-        //开始注册逻辑
+        // 开始注册逻辑
         user = UserFactory.register(model.getAccount(),
                 model.getPassword(),
                 model.getName());
 
         if (user != null) {
-            //如果有携带PushId
-            if(!Strings.isNullOrEmpty(model.getPushId())){
-                return bind(user,model.getPushId());
+
+            // 如果有携带PushId
+            if (!Strings.isNullOrEmpty(model.getPushId())) {
+                return bind(user, model.getPushId());
             }
-            AccountRspModel rspModel  =new AccountRspModel(user);
+
+            // 返回当前的账户
+            AccountRspModel rspModel = new AccountRspModel(user);
             return ResponseModel.buildOk(rspModel);
-        }else{
+        } else {
+            // 注册异常
             return ResponseModel.buildRegisterError();
         }
-
     }
 
 
-
+    // 绑定设备Id
     @POST
     @Path("/bind/{pushId}")
+    // 指定请求与返回的相应体为JSON
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    //pushID 从URL 地址中获取
-    public ResponseModel<AccountRspModel> bind (@HeaderParam("token") String token,
-                                                @PathParam("pushId") String pushID){
-        if(Strings.isNullOrEmpty(token)||Strings.isNullOrEmpty(pushID)){
+    // 从请求头中获取token字段
+    // pushId从url地址中获取
+    public ResponseModel<AccountRspModel> bind(@HeaderParam("token") String token,
+                                               @PathParam("pushId") String pushId) {
+        if (Strings.isNullOrEmpty(token) ||
+                Strings.isNullOrEmpty(pushId)) {
+            // 返回参数异常
             return ResponseModel.buildParameterError();
         }
 
-        User user = UserFactory.findByToken(token);
-        if(user != null){
-            return  bind(user,pushID);
-        }else{
-            return ResponseModel.buildAccountError();
-        }
+        // 拿到自己的个人信息
+        // User user = UserFactory.findByToken(token);
+        User self = getSelf();
+        return bind(self, pushId);
     }
 
 
+    /**
+     * 绑定的操作
+     *
+     * @param self   自己
+     * @param pushId PushId
+     * @return User
+     */
+    private ResponseModel<AccountRspModel> bind(User self, String pushId) {
+        // 进行设备Id绑定的操作
+        User user = UserFactory.bindPushId(self, pushId);
 
-    private ResponseModel<AccountRspModel> bind(User self ,String pushId){
-
-        User user = UserFactory.bindPushId(self,pushId);
-
-        if(user == null){
-            //绑定失败，则是服务器异常
+        if (user == null) {
+            // 绑定失败则是服务器异常
             return ResponseModel.buildServiceError();
+
         }
 
-
-        AccountRspModel rspModel = new AccountRspModel(user,true);
-        return  ResponseModel.buildOk(rspModel);
-
+        // 返回当前的账户, 并且已经绑定了
+        AccountRspModel rspModel = new AccountRspModel(user, true);
+        return ResponseModel.buildOk(rspModel);
 
     }
-
-
-
 }
